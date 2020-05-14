0876c470ce79d2436e41010e57008658bb262104
aliwo <aliwo@naver.com>
2020-05-14 16:11:47 +0900

    다듬기

--- api/rest/user.py
+++ api/rest/user.py
@@ -3,13 +3,12 @@ from sqlalchemy.orm.exc import NoResultFound
 from api.models.user import User
 from libs.database.engine import Session
 from libs.route.errors import ClientError
+from libs.route.router import route
 from libs.status import Status
 
 
+@route
 def get_user_profile(user_id):
-    if user_id is None:
-        raise ClientError(f'No User Found id #{user_id}', status_code=Status.HTTP_404_NOT_FOUND)
-
     try:
         user = Session().query(User).filter((User.id == user_id)).one()
     except NoResultFound:
@@ -18,5 +17,6 @@ def get_user_profile(user_id):
     return {'user': user.json()}, Status.HTTP_200_OK
 
 
+@route
 def put_user_profile():
     pass
