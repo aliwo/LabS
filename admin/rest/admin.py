e0aa461bf7eebed67a0693cc5f65d6975808abf8
aliwo <aliwo@naver.com>
2020-05-29 18:26:52 +0900

    관리자 info 까지 setup

--- admin/rest/admin.py
+++ admin/rest/admin.py
@@ -1,11 +1,12 @@
 import hashlib
 
-from flask import request
+from flask import request, g
 from sqlalchemy.orm.exc import NoResultFound
 
 from admin.handlers.auth import get_or_create_admin_session
 from admin.models.admin import Admin
 from libs.database.engine import Session
+from libs.route.login_required import login_required
 from libs.route.router import route
 from libs.status import Status
 
@@ -21,3 +22,10 @@ def login():
     Session().commit()
 
     return {'token':admin_session.token, 'okay':True}, Status.HTTP_200_OK
+
+
+@route
+def info():
+    admin = Session().query(Admin).filter((Admin.user_id == g.user_session.user.id)).one()
+    return {'admin': admin.json()}, Status.HTTP_200_OK
+
