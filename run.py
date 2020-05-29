21365bb0b44fdf8ada6d99d12e86768ebf8e6298
aliwo <aliwo@naver.com>
2020-05-29 10:35:43 +0900

    admin app 추가

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from api.app import app as api_app
-
+from admin.app import app as admin_app
 
 if __name__ == '__main__':
-    api_app.run(host='0.0.0.0', port=8000) 
+    admin_app.run(host='0.0.0.0', port=8000)
