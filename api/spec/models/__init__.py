0d789a183df9c0d58247b88aa0c7d849fbcfa1c0
aliwo <aliwo@naver.com>
2020-06-12 14:56:11 +0900

    OAS comment 추가

--- api/spec/models/__init__.py
+++ api/spec/models/__init__.py
@@ -14,6 +14,10 @@ user = convert(User, 'user.yaml', {'user':{
             'ideal_type_groups': {
                 'type': 'string',
                 'description': '(comma separated)list of integers'
+            },
+            'body_shape': {
+                'type': 'string',
+                'description': 'slim tan tan :)'
             }
         }
     }
