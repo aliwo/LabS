5fbce16f72e2f6913f5104c56500145c22653431
aliwo <aliwo@naver.com>
2020-07-01 21:09:15 +0900

    es 연결...

--- libs/elastic.py
+++ libs/elastic.py
@@ -1,2 +1,12 @@
+import os
 from elasticsearch import Elasticsearch
-es = Elasticsearch(hosts=['192.168.219.53'])
+
+option = {'hosts': [os.environ.get('SY_ELASTIC_HOST')]}
+if os.environ.get('SY_STAGE', '') == 'PRODUCTION':
+    option['use_ssl'] = True
+    option['ca_certs'] = os.environ.get('SY_ELASTIC_CA_PATH')
+
+es = Elasticsearch(**option)
+
+print('es 연결!')
+print(es.info())
