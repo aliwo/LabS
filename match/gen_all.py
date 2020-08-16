1df9188c2bf09093e003e09125847e63ce86b34d
SeungWon Jeong <aliwo@naver.com>
2020-08-17 00:42:50 +0900

    random 매치 gen 생성, helm 주석처리

--- match/gen_all.py
+++ match/gen_all.py
@@ -1,14 +1,18 @@
 import sys
 import os
+
+from match.gen_random_match import gen_random_matches
+
 sys.path.insert(0, os.path.abspath('.'))
 
 from match.gen_preference_matches import gen_preference_matches
 from match.gen_sy_matches import gen_sy_matches
-# TODO: 랜덤 매칭도 추가하기
+
 
 def gen_all():
     gen_sy_matches()
     gen_preference_matches()
+    gen_random_matches()
 
 if __name__ == '__main__':
     gen_all()
