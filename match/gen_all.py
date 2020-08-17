067d81479f1e0d50b3ef8beb5be8baed5f1f0475
SeungWon Jeong <aliwo@naver.com>
2020-08-17 10:00:50 +0900

    버그픽스

--- match/gen_all.py
+++ match/gen_all.py
@@ -1,12 +1,9 @@
 import sys
 import os
-
-from match.gen_random_match import gen_random_matches
-
 sys.path.insert(0, os.path.abspath('.'))
-
 from match.gen_preference_matches import gen_preference_matches
 from match.gen_sy_matches import gen_sy_matches
+from match.gen_random_match import gen_random_matches
 
 
 def gen_all():
