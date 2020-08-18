13267521498a8c52b0a26c018e7257308858428e
SeungWon Jeong <aliwo@naver.com>
2020-08-19 01:13:18 +0900

    cors 장착

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from swagger_ui_bundle import swagger_ui_3_path
 import connexion
 import prance
+from flask_cors import CORS
 from typing import Any, Dict
 from pathlib import Path
 import sentry_sdk
@@ -16,6 +17,7 @@ if os.environ.get('SY_STAGE', '') == 'PRODUCTION':
 
 app = connexion.App(__name__, specification_dir='api/spec/', options={'swagger_path': swagger_ui_3_path})
 set_session_destroyer(app.app)
+CORS(app.app)
 
 
 def get_bundled_specs(main_file: Path) -> Dict[str, Any]:
