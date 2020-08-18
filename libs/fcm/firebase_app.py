c2a1637f2047a4693b7354dde3497cd1b953ee30
SeungWon Jeong <aliwo@naver.com>
2020-08-18 11:23:47 +0900

    fcm_token 테스트

--- libs/fcm/firebase_app.py
+++ libs/fcm/firebase_app.py
@@ -7,8 +7,13 @@ path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'firebase_key.js
 cred = credentials.Certificate(path)
 firebase_admin.initialize_app(cred)
 
-messaging.send(messaging.Message(data={
-        'kind': 'TEST',
-        'title': f'테스트용 title',
-        'body': f'테스트용 body'
-}, token=''))
+# registration_token = 'cg0axMYwTG24eTpSX5MKn7:APA91bGkl40GfKMfoA1TgMYhsGjluZZxCew0I2uD32TQJxDRWCss-yKOt6K170Umna_Ctx87AqmAQnKdEPj2ywXkgKOz79GC8oVi7WcO-T_4RNHyVhtRbgk9ErpoJqHTmW1W8Kvt7-8G'
+# message = messaging.Message(
+#     data={
+#         'score': '850',
+#         'time': '2:45',
+#     },
+#     token=registration_token,
+# )
+# response = messaging.send(message)
+# print('Successfully sent message:', response)
