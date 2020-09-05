46cee5d8926b336068c0b4cf5aaea6845d69d7dd
SeungWon Jeong <aliwo@naver.com>
2020-09-05 14:19:25 +0900

    잘못 고친 거였네

--- match/gen_all.py
+++ match/gen_all.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.')) # 현재 디렉터리가 Labs (루트)인 상태에서 실행하기 때문에 . 을 sys.path 에 추가
 from match.gen_preference_matches import gen_preference_matches
 from match.gen_sy_matches import gen_sy_matches
 from match.gen_random_match import gen_random_matches
